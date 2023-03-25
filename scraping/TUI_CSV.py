8730d74e6af4833ef831ddbff2b74ea098a0dac9
Levi Matthijs <levi.matthijs@student.hogent.be>
2023-03-25 15:02:31 +0100

    tui afgewerkt en heeft optimale snelheid

--- scraping/TUI_CSV.py
+++
@@ -1,5 +0,0 @@
-import pandas as  pd
-
-df = pd.read_json('jsonData/tuiScrapeJsonData.json')
-
-df.to_csv('csvData/data.csv', index=None)
--- scraping/TUI_Scrape.py
+++ scraping/TUI_Scrape.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import NoSuchElementException  
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -8,100 +8,141 @@ from selenium.webdriver.support import expected_conditions as EC
 # from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import json
+import csv
 
 PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 
 url = "http://www.tuifly.be/flight/nl/"
-url2 = "http://www.tuifly.be/flight/nl/search?flyingFrom%5B%5D={0}&flyingTo%5B%5D={1}&depDate={2}&adults=1&children=0&childAge=&choiceSearch=true&searchType=pricegrid&nearByAirports=true&currency=EUR&isOneWay=true"
+url2 = "http://www.tuifly.be/flight/nl/search?flyingFrom%5B%5D=BRU&flyingTo%5B%5D={0}&depDate={1}&adults=1&children=0&childAge=&choiceSearch=true&searchType=pricegrid&nearByAirports=true&currency=EUR&isOneWay=true"
 
-departureAirports = ['ANR', 'OST', 'LGG', 'BRU']
-arrivalAirports = ['CFU', 'HER', 'RHO', 'BDS', 'NAP', 'PMO', 'FAO', 'ALC', 'IBZ', 'AGP', 'SPC', 'TFS']
-arrivalAirportsTest = ['CFU', 'HER', 'RHO', 'BDS'] # voor te testen
+departureAirports = {'Brussel', 'Antwerpen', 'Luik', 'Brugge-Oostende'}
+arrivalAirports = ['CFU', 'ALC', 'HER', 'RHO', 'BDS', 'NAP', 'PMO', 'FAO', 'AGP', 'SPC', 'TFS', 'IBZ']
 
-# DATES FOR URL
-# if we scrape data from tui we get the data of whole week, so if we need to start from the first of april
-# we have to use april 4th instead of april 1st.
-arrayWithAllDates = []
-dates = pd.date_range(start='2023-04-04', end='2023-10-03', freq='7D') # gaat maar tot 29 sept dus 2023-09-30 en 2023-10-01 moeten er nog bij
+dates = pd.date_range(start='2023-04-04', end='2023-10-03', freq='7D')
+datesPerDay = pd.date_range(start='2023-04-01', end='2023-10-01', freq='1D')
 
-alleVluchten = []
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument('--ignore-certificate-errors')
+driver_service = Service(executable_path=PATH)
 
-for datum in dates:
-    dateStr = str(datum)
-    array = dateStr.split()
-    arrayWithAllDates.append(array[0])
 
-for depAirport in departureAirports:
-    for arriAirport in arrivalAirportsTest:
-        for date in arrayWithAllDates:
+arrayWithAllDates = [datum.strftime('%Y-%m-%d') for datum in dates]
+arrayWithAllDatesPerDay = [datum.strftime('%Y-%m-%d') for datum in datesPerDay]
 
-            print('------DEBUG------')
-            print(url2.format(depAirport, arriAirport, date))
-            print(depAirport, arriAirport)
-            print(date)
-            print('-------------')
+headersCsv = ["datumVanVertrek", "vertrekLuchthaven", "aankomstLuchthaven", "vertrekTijd", "aankomstTijd", "prijs", "duur", "vrijeZitplaatsen", "vluchtNummer", "aantalStops"]
+with open('csv/tui.csv', mode='a', newline="") as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(headersCsv)
 
-            try:
-                options = webdriver.ChromeOptions()
-                options.add_experimental_option("detach", True)
-                options.add_argument('--ignore-certificate-errors')
-                driver_service = Service(executable_path=PATH)
-                driver = webdriver.Chrome(service=driver_service,options=options)
-                driver.maximize_window()
-                driver.implicitly_wait(25)
-                driver.get(url)
-                driver.find_element(By.CSS_SELECTOR, "#cmCloseBanner").click()
-                element = WebDriverWait(driver, 50).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, "div#page div.container footer > script"))
-                    ) 
-                driver.get(url2.format(depAirport, arriAirport, date))
-                data = driver.execute_script("return JSON.stringify(searchResultsJson)")
-                json_object = json.loads(data)
+i = 0
 
-                lengteFlightViewDataArray = len(json_object['flightViewData']) - 1
+for arriAirport in arrivalAirports:
+    for date in arrayWithAllDates:
 
-                while lengteFlightViewDataArray != -1:
+        print('------DEBUG------')
+        print(url2.format(arriAirport, date))
+        print("BRU", arriAirport)
+        print(date)
+        print('-----------------')
+        
+        correctURL = url2.format(arriAirport, date)
 
-                    lengteFlightViewDataFlightsectorsArray = len(json_object['flightViewData'][lengteFlightViewDataArray]['flightsectors']) - 1
-                    aantalStopsLengte = len(json_object['flightViewData'][lengteFlightViewDataArray]['flightsectors']) - 1
+        try:
+            driver = webdriver.Chrome(options=options)
+            driver.implicitly_wait(25)
+            driver.get(url)
+            driver.find_element(By.CSS_SELECTOR, "#cmCloseBanner").click()
+            element = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div#page div.container footer > script")))
+            driver.get(correctURL)
+            data = driver.execute_script("return JSON.stringify(searchResultsJson)")
+            json_object = json.loads(data)
 
-                    array = json_object['flightViewData'][lengteFlightViewDataArray]['journeySummary']
-                    
-                    vertrekDatum = array['departDate']
-                    vertrek = array['departAirportName']
-                    aankomst = array['arrivalAirportName']
-                    vertrekTijd = array['depTime']
-                    aankomstTijd = array['arrivalTime']
-                    prijs = json_object['flightViewData'][lengteFlightViewDataArray]['perPersonPrice']
-                    duur = array['totalJnrDuration']
-                    vrijeZitplaatsen = array['availableSeats']
-                    journeyType = array['journeyType']
+            alleVluchtenList = json_object['flightViewData'] # alle vluchten die beschikbaar zijn in een bepaalde week
+            weekMetOutboundDataList = json_object['dateAvailabilityData']['outboundAvailabilityData'] # elke dag van de week met data in of dat een vlucht beschikbaar is of niet
 
-                    while lengteFlightViewDataFlightsectorsArray != -1:
-                        vluchtnummer = json_object['flightViewData'][lengteFlightViewDataArray]['flightsectors'][lengteFlightViewDataFlightsectorsArray]['flightNumber']
+            while i < len(weekMetOutboundDataList):
+                datumInWeek = weekMetOutboundDataList[i]['displayDate']
+                if weekMetOutboundDataList[i]['available'] == True:
+                    j = 0
+                    alleVertrekLuchthaven = [] # lege lijst waar alle luchthavens inkomen van die dag inkomen
+                    aantalVluchtenVoorEenDatum = 0
+                    while j < len(alleVluchtenList):
+                        if datumInWeek == alleVluchtenList[j]['departureDate']: # alle vluchten van de dag met datum: datumInWeek
 
-                        vlucht = {
-                            'vertrekDatum': vertrekDatum,
-                            'vertrekPlaats': vertrek,
-                            'aankomstPlaats': aankomst,
-                            'vertrekTijd': vertrekTijd,
-                            'aankomstTijd': aankomstTijd,
-                            'prijs': prijs,
-                            'duur': duur,
-                            'vrijeZitplaatsen': vrijeZitplaatsen,
-                            'vluchtnummer': vluchtnummer,
-                            'aantalStops': aantalStopsLengte
-                        }
+                            datumVanVertrek = alleVluchtenList[j]['departureDate']
+                            vertrekLuchthaven = alleVluchtenList[j]['journeySummary']['departAirportName']
+                            aankomstLuchthaven = alleVluchtenList[j]['journeySummary']['arrivalAirportName']
+                            vertrekTijd = alleVluchtenList[j]['journeySummary']['depTime']
+                            aankomstTijd = alleVluchtenList[j]['journeySummary']['arrivalTime']
+                            prijs = alleVluchtenList[j]['totalPrice']
+                            duur = alleVluchtenList[j]['journeySummary']['journeyDuration']
+                            vrijeZitplaatsen = alleVluchtenList[j]['journeySummary']['availableSeats']
+                            vluchtNummer = alleVluchtenList[j]['flightsectors'][0]['flightNumber']
+                            aantalStops = len(alleVluchtenList[j]['flightsectors']) - 1
 
-                        lengteFlightViewDataFlightsectorsArray = lengteFlightViewDataFlightsectorsArray - 1
+                            alleVertrekLuchthaven.append(vertrekLuchthaven)
 
-                    alleVluchten.append(vlucht)
+                            with open('csv/tui.csv', mode='a', newline="") as csvFile:
+                                writer = csv.writer(csvFile)
+                                writer.writerow([datumVanVertrek, vertrekLuchthaven, aankomstLuchthaven, vertrekTijd, aankomstTijd, prijs, duur, vrijeZitplaatsen, vluchtNummer, aantalStops])
+                        j += 1
+                    # print(f'alle vertrek luchthavens met datum: ' + datumInWeek + ' list:', set(alleVertrekLuchthaven))
+                    overblijfendeLuchthavens = departureAirports.symmetric_difference(set(alleVertrekLuchthaven))
+                    if len(overblijfendeLuchthavens) != 0:
+                        for luchthaven in overblijfendeLuchthavens:
+                            datumVanVertrek = datumInWeek
+                            vertrekLuchthaven = luchthaven
+                            aankomstLuchthaven = json_object['arrAirportData'][0]['name']
+                            vertrekTijd = "geen"
+                            aankomstTijd = "geen"
+                            prijs = "geen"
+                            duur = "geen"
+                            vrijeZitplaatsen = "geen"
+                            vluchtNummer = "geen"
+                            aantalStops = "geen"
+                            with open('csv/tui.csv', mode='a', newline="") as csvFile:
+                                writer = csv.writer(csvFile)
+                                writer.writerow([datumVanVertrek, vertrekLuchthaven, aankomstLuchthaven, vertrekTijd, aankomstTijd, prijs, duur, vrijeZitplaatsen, vluchtNummer, aantalStops])
+                else:
 
-                    with open("jsonData/tuiScrapeJsonData.json", "w") as file:
-                        json.dump(alleVluchten, file, indent=4)
+                    alleVertrekLuchthaven2 = []
 
-                    lengteFlightViewDataArray = lengteFlightViewDataArray - 1
-                driver.close()
+                    datumVanVertrek = datumInWeek
+                    vertrekLuchthaven = json_object['depAirportData'][0]['name']
+                    aankomstLuchthaven = json_object['arrAirportData'][0]['name']
+                    vertrekTijd = "geen"
+                    aankomstTijd = "geen"
+                    prijs = "geen"
+                    duur = "geen"
+                    vrijeZitplaatsen = "geen"
+                    vluchtNummer = "geen"
+                    aantalStops = "geen"
 
-            except NoSuchElementException as e:
-                print(e)
+                    alleVertrekLuchthaven2.append(vertrekLuchthaven)
+
+                    with open('csv/tui.csv', mode='a', newline="") as csvFile:
+                        writer = csv.writer(csvFile)
+                        writer.writerow([datumVanVertrek, vertrekLuchthaven, aankomstLuchthaven, vertrekTijd, aankomstTijd, prijs, duur, vrijeZitplaatsen, vluchtNummer, aantalStops])
+
+                    overblijfendeLuchthavens = departureAirports.symmetric_difference(set(alleVertrekLuchthaven2))
+                    if len(overblijfendeLuchthavens) != 0:
+                        for luchthaven in overblijfendeLuchthavens:
+                            datumVanVertrek = datumInWeek
+                            vertrekLuchthaven = luchthaven
+                            aankomstLuchthaven = json_object['arrAirportData'][0]['name']
+                            vertrekTijd = "geen"
+                            aankomstTijd = "geen"
+                            prijs = "geen"
+                            duur = "geen"
+                            vrijeZitplaatsen = "geen"
+                            vluchtNummer = "geen"
+                            aantalStops = "geen"
+                            with open('csv/tui.csv', mode='a', newline="") as csvFile:
+                                writer = csv.writer(csvFile)
+                                writer.writerow([datumVanVertrek, vertrekLuchthaven, aankomstLuchthaven, vertrekTijd, aankomstTijd, prijs, duur, vrijeZitplaatsen, vluchtNummer, aantalStops])
+                i += 1
+            i = 0
+            driver.close()
+        except Exception as e:
+            print(e)
